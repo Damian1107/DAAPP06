00025f927acb8ef7e2c803bdeaec7607cb5d1b03
Damian1107 <demianx1993@gmail.com>
2019-09-26 22:05:57 -0300

    proyecto damian aquino

---
+++ app/src/main/java/com/example/daapp02/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.example.daapp02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class Main2Activity extends AppCompatActivity {
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent button2 = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(button2);
+            }
+        });
+}
+}
